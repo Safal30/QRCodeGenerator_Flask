b809f07488e504387e7bdca6223a4276fb87cd2d
Safal Sapkota <49337062+Safal30@users.noreply.github.com>
2021-09-08 15:40:15 +1000

    Commit main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, app, render_template, request
+from flask import Flask, app, render_template, request, flash
 import os, qrcode, time, base64, requests, json, io
 from PIL import Image
 
 #to create a requirements.txt = pip3 freeze > requirements.txt
 
 app = Flask(__name__)
+app.secret_key = "12ab&34cd69po&"
 
 UPLOAD_FOLDER = "/"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
@@ -20,27 +21,36 @@ def form():
 def data():
 
     text = request.form["title"]
-    #pro_text = text.upper()
 
     url = text
 
-    #an instance of qrcode
-    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4,)
+    #checking the length of the chars
+    if len(url) > 1000:
 
-    qr.add_data(url)
-    qr.make(fit=True)
+        flash("Character count too big", "error")
 
-    img = qr.make_image(fill_color="black", back_color="white")
-    #img.save("static\qrcode.png")
+        return render_template("base.html")
+        
 
-    data = io.BytesIO()
-    img.save(data, "JPEG")
-    encoded_img_data = base64.b64encode(data.getvalue())
+    else: 
 
-    return render_template("qrcode.html", img = encoded_img_data.decode("utf-8"))
+        #an instance of qrcode
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4,)
+
+        qr.add_data(url)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="black", back_color="white")
+        #img.save("static\qrcode.png")
+
+        data = io.BytesIO()
+        img.save(data, "JPEG")
+        encoded_img_data = base64.b64encode(data.getvalue())
+
+        flash("QR Code Generated Successfully", "success")
+
+        return render_template("qrcode.html", img = encoded_img_data.decode("utf-8"))
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
